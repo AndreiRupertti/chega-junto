6fb9fe2dcb28f75b1b0159b16f6ab89790b0296c
AndreiRupertti <arupertti@gmail.com>
2018-02-04 23:05:10 -0200

    adiona testes para as rotas da api

--- server/src/app.js
+++ server/src/app.js
@@ -2,9 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const dbController = require('./controllers/db_controller')
-const Person = require('./models/Person')
-const Oraganization = require('./models/Organization')
+const routes = require('./routes/routes')
+
 const app = express()
 
 mongoose.connect('mongodb://localhost:27017/chega-junto')
@@ -14,38 +13,12 @@ app.set('port', (process.env.PORT || 3000))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/findEvents', (req, res) => {
-  Oraganization.find()
-    .then(allOrgs => {
-      res.send({allOrgs})
-    })
-    .catch(err => {
-      console.log(err)
-    })
-})
-app.get('/findEventById', (req, res) => {
-  // console.log(req.body.cardId)
-  Oraganization.find({'events._id': req.body.id})
-    .then(event => {
-      console.log(event)
-      res.send({event})
-    })
-    .catch(err => {
-      console.log(err)
-    })
-})
+app.get('/findOrgs', routes.findOrgs)
+
+app.post('/findEventById/:id', routes.findEventById)
 
 app.post('/insertItem', (req, res) => {
-  const personController = dbController(Person)
 
-  personController.findAll()
-    .then(response => {
-      console.log(response)
-      res.send({response})
-    })
-    .catch(err => {
-      console.log(err)
-    })
 })
 
 app.listen(app.get('port'), () =>

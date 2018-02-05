01ef93dd68093d5e3c905445c02145bfd7f15547
AndreiRupertti <arupertti@gmail.com>
2018-02-05 17:49:30 -0200

    altera baseURl na Api

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    // baseURL: (process.env.API_URL || `http://localhost:3000/`)
-    baseURL: process.env.API_URL
+    // baseURL: process.env.API_URL || `http://localhost:3000/`
+    baseURL: (process.env.NODE_ENV === 'production') ? process.env.API_URL : `http://localhost:3000/`
   })
 }

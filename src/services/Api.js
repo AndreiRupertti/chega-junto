44773b69d7b360d78208a217b98b41a0eff4bffe
AndreiRupertti <arupertti@gmail.com>
2018-02-07 03:39:07 -0200

    altera url da api em produção

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    // baseURL: process.env.API_URL || `http://localhost:3000/`
-    baseURL: (process.env.NODE_ENV === 'production') ? process.env.API_URL : `http://localhost:3000/`
+    baseURL: (process.env.NODE_ENV === 'production') ? `https://api-chegajunto-staging.herokuapp.com/` : `http://localhost:3000/`
+    // baseURL: (process.env.NODE_ENV === 'production') ? process.env.API_URL : `http://localhost:3000/`
   })
 }
